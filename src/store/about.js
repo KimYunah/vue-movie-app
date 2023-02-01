506b8a5e74352e826931c5cadc3aa24871d22e61
KimYunah <kya38317@gmail.com>
2023-02-01 09:31:19 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'aaa',
+    email: 'aaa@aaa.com',
+    blog: 'https://aaa.blog',
+    phone: '+82-10-1111-1111',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
